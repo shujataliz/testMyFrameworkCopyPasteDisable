9d4733ac34f76a8f34d491b539d4d6fe96635335
Shujat Ali <Shujat.dev@gmail.com>
2023-12-08 12:42:06 -0600

    tested with background screen prevention feature

--- testMyFramework/AppDelegate.swift
+++ testMyFramework/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         MyFramework.disableCopyPasteSwizzleEnterprise()
-        MyFrameClass.disableScreenRecording()
+        MirrorHelper.disableScreenRecording()
+        MySplashHelper.registerSplashScreenforBackground()
         
         
         return true
